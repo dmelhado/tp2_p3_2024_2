531338b93224baf389017e13bb9acbcfcd9bb3ae
Ignacio Aranda Bao <ignacioarandabao@hotmail.com>
2024-10-03 16:29:56 -0300

    spy

---
+++ src/weightedGraph/Spy.java
@@ -0,0 +1,15 @@
+package weightedGraph;
+
+public class Spy {
+    private String name;
+
+    public Spy(String name) {
+        this.name = name;
+    }
+
+	public String getName() {
+        return name;
+    }
+}
+
+
