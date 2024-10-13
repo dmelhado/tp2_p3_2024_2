1649c1031b508f08411bb6328e39c7d5507bfbca
user <emasuarez12@gmail.com>
2024-10-13 14:18:49 -0300

    verificación bucle

--- src/controller/VentanaConexionesControlador.java
+++ src/controller/VentanaConexionesControlador.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 import javax.swing.JOptionPane;
 
 import view.VentanaConexiones;
@@ -9,35 +8,30 @@ import view.VentanaMenu;
 public class VentanaConexionesControlador {
 	private VentanaConexiones ventanaConexiones;
 	private VentanaMenu ventanaMenu;
-	
-	public VentanaConexionesControlador(VentanaConexiones ventanaConexiones, VentanaMenu ventanaMenu)
-	{
+
+	public VentanaConexionesControlador(VentanaConexiones ventanaConexiones, VentanaMenu ventanaMenu) {
 		this.ventanaConexiones = ventanaConexiones;
 		this.ventanaMenu = ventanaMenu;
-		
+
 		this.ventanaConexiones.setControlador(this);
 	}
 
 	public void agregarArista(String espia1, String espia2, String probabilidadTexto) {
-		if (espia1 != null && espia2 != null && !espia1.equals(espia2)) {
-			if (ventanaMenu.getControlador().existeConexion(espia1, espia2)) {
-	            JOptionPane.showMessageDialog(
-	                ventanaMenu,
-	                "La conexión entre '" + espia1 + "' y '" + espia2 + "' ya existe.",
-	                "Error de Conexión",
-	                JOptionPane.WARNING_MESSAGE
-	            );
-	        } else {
+		if (espia1 == null && espia2 == null || espia1.equals(espia2)) {
+			JOptionPane.showMessageDialog(ventanaMenu, "No se admiten Bucles o no se seleccionaron Espias",
+					"Error de Conexión", JOptionPane.WARNING_MESSAGE);
+			return;
+		}
+		if (ventanaMenu.getControlador().existeConexion(espia1, espia2)) {
+			JOptionPane.showMessageDialog(ventanaMenu,
+					"La conexión entre '" + espia1 + "' y '" + espia2 + "' ya existe.", "Error de Conexión",
+					JOptionPane.WARNING_MESSAGE);
+		} else {
 			Double.parseDouble(probabilidadTexto.replace(",", "."));
 			ventanaMenu.agregarTablaAristaProbabilidad(espia1, espia2, probabilidadTexto);
-	        }
 		}
-		
-		
-		
 	}
-	
-	
-	
 
 }
+
+
